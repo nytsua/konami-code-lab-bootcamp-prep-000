d6f56cad9ff154d8353c16877cf2d865b79c79cc
Austyn Trace <atrace@gmail.com>
2017-09-04 14:53:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,11 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   const divs = document.querySelectorAll('div');
-  let index = -1;
+  let index = 0;
 
   function checkCode(e) {
-    
+    const key = parseInt(e.detail || e.which);
+    if (parseInt(key) === code[index]) {
+      index++;
+ 
+      if (index === alphabet.length) {
+        alert('Good job!');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
   }
 
-  document.body.addEventListener('keydown', checkCode);  
+  document.body.addEventListener('keydown', checkCode);
 }
