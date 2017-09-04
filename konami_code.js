43b7e9b6435eeed3195288954808e300b660b129
Austyn Trace <atrace@gmail.com>
2017-09-04 14:50:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,16 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const divs = document.querySelectorAll('div')
+ 
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation()
+ 
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  document.body.addEventListener('click', bubble)
+}
 }
